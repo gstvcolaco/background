d15c0755d609e3959416b5516c7d3f17b8de0694
gstvcolaco <124811513+gstvcolaco@users.noreply.github.com>
2023-08-04 16:47:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function primeira (){
+    document.body.style.backgroundImage = "url('https://mundoconectado.com.br/uploads/2022/05/25/25658/cacto.jpg')";
+}
+function segunda (){
+    document.body.style.backgroundImage="url('https://img.freepik.com/fotos-gratis/uma-imagem-de-um-planeta-com-um-buraco-negro-no-centro-e-um-buraco-negro-no-centro_1340-23795.jpg')";
+}
+function terceira (){
+    document.body.style.backgroundImage="url('https://www.interligar.com.br/wp-content/uploads/2017/10/artigo_01102017.jpg')";
+}
